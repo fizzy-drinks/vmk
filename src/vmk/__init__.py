a2ee0251343092c2e6e5e7a6cfa2da17edc5bbe0
Gabriel Chiconi <gabriel.chiconi@netdocuments.com>
2024-04-03 17:00:32 -0300

    Add file select, dir select and output options

--- src/vmk/__init__.py
+++ src/vmk/__init__.py
@@ -1,45 +1,33 @@
-import PyPDF2, os
+import argparse
+from .combiner import Combiner
+
+
+DEFAULT_OUT_FILE = 'CombinedFirstPages1.pdf'
+DEFAULT_DIR = './'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--out', help="Output file")
+
+input_type = parser.add_mutually_exclusive_group()
+input_type.add_argument('-f', '--files', help="Input files", nargs='+')
+input_type.add_argument('-d', '--dir', help="Input directory", nargs='+')
+
 
 def main():
-    your_target_folder = "./"
+    args = parser.parse_args()
+    combiner = Combiner()
+    out = args.out or DEFAULT_OUT_FILE
+    
+    if args.files:
+        for f in args.files:
+            combiner.add_file(f)
 
-    if os.path.exists("CombinedFirstPages1.pdf"):
-        os.remove("CombinedFirstPages1.pdf")
-    else:
-        pass
-
-    pdf_files = []
-
-    for dirpath, _, filenames in os.walk(your_target_folder):
-
-        _.clear() #remove subpastas e seus arquivos
-
-    #    print(dirpath)
-    #    print(_)
-    #    for n in filenames:
-    #        print(n)
-
-        for items in filenames:
-
-            file_full_path = os.path.abspath(os.path.join(dirpath, items))
-
-            if file_full_path.lower().endswith(".pdf"):
-                pdf_files.append(file_full_path)
-
-            else:
-                pass
-
-    pdf_files.sort(key=str.lower)
-    pdfWriter = PyPDF2.PdfWriter()
-
-    for files_address in pdf_files:
-        pdfFileObj = open(files_address, 'rb')
-        pdfReader = PyPDF2.PdfReader(pdfFileObj)
-
-        pageObj = pdfReader.pages[0]
-        pdfWriter.add_page(pageObj)
-
-    with open("CombinedFirstPages1.pdf", "wb") as output:
-        pdfWriter.write(output)
-
-    print('done1')
+        combiner.assemble()
+        combiner.write_pdf(out)
+        print('Combined files into', out)
+        return
+    
+    target_dir = args.dir or DEFAULT_DIR
+    combiner.combine_pdfs_in_folder(args.dir, out)
+    print('Combined all PDF files in', args.dir, 'into', out)
